855bb403b0b7f83460d3a8d2d9c40c14dee849a3
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-01-03 13:02:28 +0200

    Learning states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoggedIn: false
+    }
+  }
+  
+  render () {
+    let wordDisplay
+    if (this.state.isLoggedIn === true) {
+      wordDisplay = 'in'
+    } else {
+      wordDisplay = 'out'
+    }
+    return (
+      <div className="App">
+        <h1>You are currently logged {wordDisplay}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
