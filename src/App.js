2413d407a2abe3d6559a6c3cb7102ed9afb4b4e8
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-01-10 13:22:40 +0200

    Consuming context

--- src/App.js
+++ src/App.js
@@ -1,65 +1,15 @@
 import React, { useState } from 'react';
-import "./App.css";
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import ThemeContextProvider from './contexts/ThemeContext';
 
-function Todo({ todo, index }) {
-  return <div className="todo">{todo.text}</div>;
-}
-
-function TodoForm({addTodo}) {
-  const [value, setValue] = useState('');
-  
-  const handleSubmit = e => {
-    e.preventDefault();
-    if(!value) return;
-    addTodo(value);
-    setValue('');
-    // console.log('hello')
-  }
-  
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-        placeholder="Add Todo..."
-      />
-    </form>
-  )
-}
-
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      text: 'Learn about React',
-      isCompleted: false
-    },
-    {
-      text: 'Meet friend for launch',
-      isCompleted: false
-    },
-    {
-      text: 'Build really cool Todo app',
-      isCompleted: false
-    }
-  ]);
-  
-  const addTodo = text => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-    // console.log(text)
-  }
-  
+function App() {  
   return (
     <div className="App">
-      <div className="todo-list">
-        {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
-        ))}
-        <TodoForm
-          addTodo={addTodo}
-        />
-      </div>
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   )
 }
