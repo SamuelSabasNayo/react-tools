5c9b6f765e41d5ef400db9acf58bd679a89b6025
unknown <nshusamuel1@gmail.com>
2026-06-24 12:20:42 +0200

    routing & not found pages

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 const ProductReview = async ({
   params,
 }: {
@@ -5,6 +7,10 @@ const ProductReview = async ({
 }
 ) => {
   const { productId, reviewId } = await params;
+
+  if (parseInt(reviewId) > 1000) {
+    notFound();
+  }
 
   return (
     <h1>Review { reviewId } for product { productId }</h1>
