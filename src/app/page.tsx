d45a95139833d1695055489f54fa33c9df328afc
unknown <nshusamuel1@gmail.com>
2026-06-29 22:13:59 +0200

    nested routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ export default async function Home() {
   return (
     <main>
       <div>
-        <h1>Welcome Home!</h1>
+        <h1 
+        // className="text-blue-800"
+        // className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+        >
+          Welcome Home!
+        </h1>
       </div>
     </main>
   );
