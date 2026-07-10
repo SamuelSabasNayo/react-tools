58211817817d52d55b1d015acd7fd16c38deb7df
unknown <nshusamuel1@gmail.com>
2026-07-11 00:07:43 +0200

    query parameters, headers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,31 @@
-export async function GET() {
-  return new Response("Profile API data")
+import { headers, cookies }  from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const headersList = await headers();
+  console.log(headersList.get("Authorization"));
+
+  const theme =  request.cookies.get("theme");
+  console.log(theme);
+  
+  const cookieStore = await cookies();
+  cookieStore.set("resultPerPage", "20");
+  console.log(cookieStore.get("resultPerPage"));
+  
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookies": "theme=dark",
+    }
+  })
 }
+
+
+// import { NextRequest } from "next/server";
+
+// export async function GET(request: NextRequest) {
+//   const requestHeaders = new Headers(request.headers);
+//   console.log(requestHeaders.get("Authorization"));
+
+//   return new Response("Profile API data")
+// }
